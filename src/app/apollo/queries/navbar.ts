a87c05b90945064a1da499a3be21f119314eecdb
Niels Ebbelink <niels.ebbelink@carnext.com>
2021-10-16 21:34:45 +0200

    - new navbar structure is used - setable navbar site name - reworked mobile navigation and search

--- src/app/apollo/queries/navbar.ts
+++ src/app/apollo/queries/navbar.ts
@@ -1,44 +1,11 @@
-import { query } from "@angular/animations";
 import gql from "graphql-tag";
-
-export const NAVBAR_QUERY = gql`
-query topLevelNavbarItems{
-  navBarItems(where: {isInNavBar:true}){
-    isInNavBar
-    title
-    path
-    nav_bar_item{
-      title
-    }
-    nav_bar_items
-    {
-      title
-      path
-      nav_bar_items
-      {
-        title
-        path
-        nav_bar_items
-        {
-          title
-          path
-        }
-      }
-    }
-  }
-}
-`;
-
-export type NAVBAR_QUERY_RESULT = {
-  navBarItems: NavbarItem[];
-}
 
 export const NAVBAR_QUERY_2 = gql`
 {
   navbar {
     SiteName
     TopLevelNavItems {
-      navItems: nav_bar_item {
+      navItem: nav_bar_item {
         title
         path
         navItems: nav_bar_items {
@@ -68,24 +35,12 @@ export class Navbar {
   }
 }
 
-export type navItems = {
+export type NavRelation = {
+  navItem: navItem;
+}
+
+export type navItem = {
   readonly title: string;
   readonly path: string;
-  navItems: navItems[];
-}
-
-export type NavRelation = {
-  navItems: navItems[];
-}
-
-export class NavbarItem {
-  readonly isInNavBar: boolean = false;
-  readonly title: string = "";
-  readonly path: string = "";
-  readonly nav_bar_item: NavbarItem | undefined = undefined;
-  readonly nav_bar_items: NavbarItem[] = [];
-
-  constructor(queryOutput: Partial<NavbarItem>) {
-    Object.assign(this, queryOutput);
-  }
+  navItems: navItem[];
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from "apollo-angular";
-import { NAVBAR_QUERY, NavbarItem, NAVBAR_QUERY_RESULT, NAVBAR_QUERY_2_RESULT, NAVBAR_QUERY_2, Navbar } from '../apollo/queries/navbar';
+import { NAVBAR_QUERY_2_RESULT, NAVBAR_QUERY_2, Navbar } from '../apollo/queries/navbar';
 import { Subscription, pipe } from "rxjs";
 
 @Component({
@@ -9,28 +9,16 @@ import { Subscription, pipe } from "rxjs";
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  navbarItems: NavbarItem[] = [];
   navbar: Navbar = new Navbar({});
   loading = true;
   errors: any;
 
-  navbarQuerySubscription: Subscription | undefined = undefined;
   navbarQuerySubscription2: Subscription | undefined = undefined;
 
   constructor(private apollo: Apollo) { }
 
   ngOnInit() {
-    this.navbarQuerySubscription = this.apollo
-      .watchQuery<NAVBAR_QUERY_RESULT>({
-        query: NAVBAR_QUERY
-      })
-      .valueChanges.subscribe(result => {
-        this.navbarItems = result.data.navBarItems;
-        this.loading = result.loading;
-        this.errors = result.errors;
-      });
-
-      this.navbarQuerySubscription2 = this.apollo
+    this.navbarQuerySubscription2 = this.apollo
       .watchQuery<NAVBAR_QUERY_2_RESULT>({
         query: NAVBAR_QUERY_2
       })
@@ -41,6 +29,6 @@ export class NavComponent implements OnInit {
       });
   }
   ngOnDestroy() {
-    this.navbarQuerySubscription?.unsubscribe();
+    this.navbarQuerySubscription2?.unsubscribe();
   }
 }
